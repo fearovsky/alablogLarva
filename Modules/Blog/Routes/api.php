378393775d839651d60b5a2260d5212a9d5e55e6
Mateusz Krzak <mateuszkrzak.web@gmail.com>
2025-02-11 21:29:15 +0100

    Add ArticleController, config api version

--- Modules/Blog/Routes/api.php
+++ Modules/Blog/Routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Blog\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/blog', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api', 'prefix' => config('app.apiVersion')], function () {
+    Route::apiResource('articles', ArticleController::class);
 });
--- config/app.php
+++ config/app.php
@@ -212,4 +212,5 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
+    'apiVersion' => env('API_VERSION', 'v1'),
 ];
